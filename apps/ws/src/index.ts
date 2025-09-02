41e386d771f5269b0d71566036762077028b504b
Utkarsh Pandey <utk2665@gmail.com>
2025-09-02 15:47:25 +0530

    Sample code for testing http and ws servers

---
+++ apps/ws/src/index.ts
@@ -0,0 +1,15 @@
+import WebSocket, { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({
+    port: 8080,
+})
+
+wss.on('connection', function connection(ws){
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+
+    ws.send('something');
+})
