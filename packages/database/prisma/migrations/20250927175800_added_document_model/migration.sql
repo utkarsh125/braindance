00dea2888c6768b41eedef7a9744231222a4fe13
Utkarsh Pandey <utk2665@gmail.com>
2025-09-27 23:28:12 +0530

    Added new migration

---
+++ packages/database/prisma/migrations/20250927175800_added_document_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "public"."Document" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" JSONB NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Document" ADD CONSTRAINT "Document_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
